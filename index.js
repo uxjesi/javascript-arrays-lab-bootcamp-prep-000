7931ca5648362d724474c14adb0afa0e58dcc964
Jesi Loredo <jesi.thegreat@gmail.com>
2018-07-11 21:55:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+var newCat = ["Broom"]
+
+function destructivelyAppendKitten(kitten) {
+ kittens.push(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten (kitten) {
+  kittens.unshift(kitten)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(newCat) {
+   
+ cats = kittens.append(newCat) 
+  
+  return cats
+}
+
+
